c28ee894082fdd773dda0883c73b2142dd21797b
Hanichen <>
2021-05-02 20:19:27 +0000

    Aufgaben Blatt 2

--- Test.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <fstream>
-
-int main{
-std::ofstream fout("daten.txt"); fout << zahl << std::endl; fout.close();
-int zahl;
-std::cin >> ;
-std::cout << zahl << std::endl
-}
